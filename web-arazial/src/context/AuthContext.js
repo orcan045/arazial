a5a8dd83d429de7a81dcbe36d2d3fd7ba149e4bd
udede11 <ufuk@diplomade.com>
2025-04-11 04:17:05 +0300

    redirect fixes

--- web-arazial/src/context/AuthContext.js
+++ web-arazial/src/context/AuthContext.js
@@ -451,9 +451,7 @@ export function AuthProvider({ children }) {
       const { data, error } = await supabase.auth.signUp({
         email,
         password,
-        options: {
-          emailRedirectTo: 'https://arazialcom.net'
-        }
+        redirectTo: 'https://arazialcom.net/',
       });
       
       if (error) throw error;
